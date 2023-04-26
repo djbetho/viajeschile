df739daaf8e494f0126413458e2b02f479574e53
Ruperto <ru.bugueno@gmail.com>
2023-04-26 17:06:08 -0400

    Primer comentario despues del INIT

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  
+    $("#enviar").click(function() {
+        alert("El correo fue enviado correctamente...");
+      });
+
+  });
